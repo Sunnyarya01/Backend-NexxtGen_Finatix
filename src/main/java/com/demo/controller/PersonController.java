25c7e1e345c4ab332978dfd37b0433a10516a165
learner01 <learnerindialearn@gmail.com>
2023-03-28 19:00:40 +0530

    done

--- src/main/java/com/demo/controller/PersonController.java
+++ src/main/java/com/demo/controller/PersonController.java
@@ -1,8 +1,14 @@
 package com.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +27,21 @@ public class PersonController {
 	@PostMapping("/")
 	public Person register(@RequestBody Person person) {
 		return personService.registerUser(person);
+	}
+	
+	@GetMapping("/")
+	public List<Person> get(){
+		return personService.getAll();
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@RequestBody Person person, @PathVariable("id") Long id) {
+		return personService.update(person, id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		return personService.delete(id); 
 	}
 	
 }
