4edd2b11ea1d86a674e739cddb074226b047c02b
learner01 <learnerindialearn@gmail.com>
2023-03-28 19:06:38 +0530

    added

--- src/main/java/com/demo/service/PersonService.java
+++ src/main/java/com/demo/service/PersonService.java
@@ -1,5 +1,8 @@
 package com.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,33 @@ public class PersonService {
 	
 	public Person registerUser(Person person) {
 		return personRepo.save(person);
+	}
+	
+	public List<Person> getAll(){
+		return personRepo.findAll();
+	}
+	
+	public String update(Person person, Long id) {
+		Optional<Person> p = personRepo.findById(id);
+		if(p.isEmpty()) {
+			return "id not found";
+		}
+		Person pp = p.get();
+		pp.setEmail(person.getEmail());
+		pp.setName(person.getName());
+		pp.setPassword(person.getPassword());
+		personRepo.save(pp);
+		return "Updated";
+	}
+	
+	public String delete(Long id) {
+		Optional<Person> p = personRepo.findById(id);
+		if(p.isEmpty()) {
+			return "id not found";
+		}
+		Person pp = p.get();
+		personRepo.delete(pp);
+		return "Deleted";
 	}
 	
 }
